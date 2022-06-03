fcb0713f2634cd35bc9704f1575101d773997892
Karen <kareninejapiassu@gmail.com>
2022-06-03 16:13:33 +0200

    Adding store and Provider

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore } from 'redux';
+
+const redux = require('redux');
+
+const counterReducer = (state = {counter:0}, action) => {
+  if(action.type === 'increment'){
+    return{
+      counter: state.counter + 1,
+    }
+  }
+  if(action.type === 'decrement'){
+    return{
+      counter: state.counter - 1,
+    }
+  }
+  return state;
+};
+
+const store = redux.configureStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
+
+export default store;
